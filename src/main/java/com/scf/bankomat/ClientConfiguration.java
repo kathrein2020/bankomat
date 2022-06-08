ff096c77ddf366b21e85578d36bbba504f62e660
Yereke <69048847+kathrein2020@users.noreply.github.com>
2022-06-09 00:42:06 +0600

    Add files via upload

---
+++ src/main/java/com/scf/bankomat/ClientConfiguration.java
@@ -0,0 +1,14 @@
+package com.scf.bankomat;
+
+import com.scf.bankomat.controller.ClientController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClientConfiguration {
+//
+//    @Bean
+//    public ClientController clientController(){
+//        return new ClientController();
+//    }
+}
